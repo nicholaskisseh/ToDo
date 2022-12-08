2a048956fd71fe4cb8c1802bb2c89aa62f36fa6a
zues <quencyraswpberry10@gmail.com>
2022-12-08 15:38:25 +0000

    added a clear functionality to the script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+let addbtn = document.getElementById('addbtn'); 
+let clearbtn = document.getElementById('clearbtn'); 
+let todo_ol = document.getElementById('mylist'); 
+var todolistarray = [];
+
+var clearlist = function(){
+todolistarray = []
+localStorage.removeItem('mylist');
+mylist.innerHTML = '';
+}
+clearbtn.addEventListener('click',clearlist);
 
