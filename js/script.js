0a313bfe3f69b3e97d73d17fb747a807395f4503
zues <quencyraswpberry10@gmail.com>
2022-12-12 14:32:10 +0000

    BUGGY SCRIPT

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let addbtn = document.getElementById('addbtn'); 
 let clearbtn = document.getElementById('clearbtn'); 
 let todo_ol = document.getElementById('mylist'); 
+let inputItem = document.getElementById('inputItem');
 var todolistarray = [];
 
 var clearlist = function(){
@@ -8,5 +9,30 @@ todolistarray = []
 localStorage.removeItem('mylist');
 mylist.innerHTML = '';
 }
-clearbtn.addEventListener('click',clearlist);
 
+var ListItemobj = function(content, status){
+    this.content = '';
+    this.status = 'incomplete'; 
+}
+
+let refreshlocalstorage = function(){
+var todo = todolistarray;
+localStorage.removeItem('my list');
+localStorage.setItem('my list', JSON.stringify(todo))
+
+}
+
+var addlistL = function(){
+   let newlistitem = new ListItemobj();
+   newlistitem.content = inputItem.value;
+   todolistarray.push(newlistitem);
+   //Refresh
+   refreshlocalstorage();
+
+   var newitem = createitemDom(inputItem.value, 'incomplete');
+   mylist.appendchild(newitem);
+   inputItem.value = '';
+}
+
+
+clearbtn.addEventListener('click',clearlist);
